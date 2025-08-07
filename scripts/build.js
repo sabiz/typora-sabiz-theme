fb51fd8259ba325057c522246601d377e06364a0
sAbIz <sabiz@users.noreply.github.com>
2025-08-07 02:47:28 +0000

    setup postcss

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,14 @@
 const fs = require('fs-extra');
-const sass = require('sass');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const postcssImport = require('postcss-import');
+const cssnano = require('cssnano');
+const path = require('path');
 
 const BUILD_TYPE_RELEASE = "release";
 const BUILD_TYPE_DEVELOP = "dev";
 
 const OUT_DIRECTORY_PATH = './dist';
-
 exports.OUT_DIRECTORY_PATH = OUT_DIRECTORY_PATH;
 
 if (require.main !== module) {
@@ -14,21 +17,31 @@ if (require.main !== module) {
 
 let buildType = process.argv[2] || BUILD_TYPE_RELEASE;
 
-let options = {};
-
-switch(buildType) {
-    case BUILD_TYPE_RELEASE:
-        options.style ='compressed';
-        break;
-    default: // BUILD_TYPE_DEVELOP
-        break;
-}
+const inputCssPath = path.resolve('./src/main.css');
+const outputCssPath = path.resolve(`${OUT_DIRECTORY_PATH}/sabiz.css`);
 
 fs.removeSync(OUT_DIRECTORY_PATH);
 
-let result = sass.compile('./src/main.scss', options);
-// console.log(result);
 fs.mkdirsSync(OUT_DIRECTORY_PATH);
-fs.writeFileSync(`${OUT_DIRECTORY_PATH}/sabiz.css`, result.css.toString());
 
-fs.copySync('./src/res', `${OUT_DIRECTORY_PATH}/sabiz`);
+const plugins = [autoprefixer(), postcssImport()];
+if (buildType === BUILD_TYPE_RELEASE) {
+    plugins.push(cssnano());
+}
+
+fs.readFile(inputCssPath, (err, css) => {
+    if (err) throw err;
+    postcss(plugins)
+        .process(css, { from: inputCssPath, to: outputCssPath })
+        .then(result => {
+            fs.writeFileSync(outputCssPath, result.css);
+            if (result.map) {
+                fs.writeFileSync(outputCssPath + '.map', result.map.toString());
+            }
+            fs.copySync('./src/res', `${OUT_DIRECTORY_PATH}/sabiz`);
+        })
+        .catch(e => {
+            console.error(e);
+            process.exit(1);
+        });
+});
